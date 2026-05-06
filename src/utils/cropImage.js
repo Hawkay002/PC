c9595be3b895631ba7af7cac74ca0503a6b5cf39
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-05-06 20:52:53 +0530

    Add function to crop images from source

---
+++ src/utils/cropImage.js
@@ -0,0 +1,32 @@
+export default async function getCroppedImg(imageSrc, pixelCrop) {
+  const image = await new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => resolve(img));
+    img.addEventListener('error', (error) => reject(error));
+    img.src = imageSrc;
+  });
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
+  );
+
+  return new Promise((resolve) => {
+    canvas.toBlob((blob) => {
+      resolve(new File([blob], 'cropped.jpg', { type: 'image/jpeg' }));
+    }, 'image/jpeg');
+  });
+}
