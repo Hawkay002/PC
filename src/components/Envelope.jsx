261d216edb8dbb5d2fee9651115ce4abc5222f32
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-05-15 12:19:00 +0530

    Update Envelope.jsx

--- src/components/Envelope.jsx
+++ src/components/Envelope.jsx
@@ -133,10 +133,10 @@ export default function Envelope({ postcardData }) {
             <motion.img
               src="/waxseal-5.webp"
               alt="Broken Wax Seal Remnant"
-              className="absolute -bottom-2 left-[10%] -translate-x-1/2 -rotate-[25deg] w-24 h-24 object-contain z-20 pointer-events-none drop-shadow-md"
+              className="absolute -bottom-2 left-[7%] -translate-x-1/2 -rotate-[25deg] w-24 h-24 object-contain z-20 pointer-events-none drop-shadow-md"
               initial={{ opacity: 0 }}
               animate={{ opacity: step >= 4 ? 1 : 0 }}
-              transition={{ duration: 0.5, delay: 1.2, ease: 'easeIn' }}
+              transition={{ duration: 0.5, delay: 1.7, ease: 'easeIn' }}
             />
           </div>
         </motion.div>
